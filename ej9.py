e3a39ab2ffe1b6324491f9b627064efe1d2ab481
agarcle1210 <andgarcle26@gmail.com>
2025-01-31 14:04:27 +0100

    ej9

--- ej9.py
+++ ej9.py
@@ -1,1 +1,13 @@
-
+lista = {"Jose": 9, "Saúl": 7, "Adri": 5}
+def notas():
+    promedio = 0
+    maximo_estudiante  = ""
+    mejor_nota = 0
+    for name, nota in lista.items():
+        if nota > mejor_nota:
+            mejor_nota = nota
+            maximo_estudiante = name
+        promedio += nota/len(lista)
+        
+    return "Promedio: ",promedio," Nota máxima: ",mejor_nota,"Mejor estudiante: ",maximo_estudiante
+print (notas())
