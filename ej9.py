82e638367b870c846416a3c923b26ac260830548
andresgc_26 <andgarcle26@gmail.com>
2025-01-31 13:32:59 +0100

    Update ej9.py

--- ej9.py
+++ ej9.py
@@ -1,9 +1,1 @@
-def calcular_promedio(estudiantes):
-    total_notas = sum(estudiantes.values())
-    promedio= total_notas/len(estudiantes)
-    
-    mejor_estudiante = max(estudiantes, key=estudiantes.get)    
-    
-    return "El promedio es: " ,promedio, ". Mejor estudiante " , mejor_estudiante , "con nota: ", estudiantes[mejor_estudiante]
 
-print (calcular_promedio({"Andres": 10, "Saúl": 3.5, "Risketo Morales": 6, "Apazram": 7}))
